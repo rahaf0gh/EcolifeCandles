81791448dbc9cc3de5fc9ba99048ac3879f3de64
rahaf0gh <162138925+rahaf0gh@users.noreply.github.com>
2025-01-21 20:32:47 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post("/decrypt", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
